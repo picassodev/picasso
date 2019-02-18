fadf93d8f4eebf3062d9bd06672400f9eeeb6158
Chong K <kc4@or-condo-login02.ornl.gov>
2019-02-18 11:57:47 -0500

    add Harlow_Tolerance to handle tolerance traits and forgot to add this file

---
+++ src/Harlow_Tolerance.hpp
@@ -0,0 +1,28 @@
+#ifndef HARLOW_TOLERANCE_HPP
+#define HARLOW_TOLERANCE_HPP
+
+#include <Kokkos_Core.hpp>
+
+namespace Harlow
+{
+//---------------------------------------------------------------------------//
+// traits class to handel Tolerance depending on data type
+//---------------------------------------------------------------------------//
+template<typename T>
+struct Tolerance;
+
+template<>
+struct Tolerance<float>
+{
+   static constexpr float tol = 1e-6;
+};
+
+template<>
+struct Tolerance<double>
+{
+   static constexpr double tol = 1e-15;
+};
+
+} // end namespace Harlow
+
+#endif // end HARLOW_TOLERANCE_HPP
